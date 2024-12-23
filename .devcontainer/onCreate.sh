edd7116c682967f15150416b459950553c5be3cc
Niko Böckerman <git.585wj@slmail.me>
2024-12-23 12:10:31 +0000

    Use yarn

--- .devcontainer/onCreate.sh
+++ .devcontainer/onCreate.sh
@@ -2,6 +2,9 @@
 
 set -eux
 
+yarn
+yarn dlx @yarnpkg/sdks vscode
+
 if [ -e CMakeUserPresets.json ]; then
     echo "CMakeUserPresets.json already exists."
     echo "Take a look at .devcontainer/CMakeUserPresets.json for prepared presets to use in devcontainer."
